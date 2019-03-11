21a6e160e707d1a59337a297482dafd9a06e3c4d
mspake <morgana.spake@gmail.com>
2019-03-11 14:26:43 -0700

    one object built, able to print to page

--- js/sales.js
+++ js/sales.js
@@ -1,1 +1,16 @@
 'use strict';
+
+//object hold the info for the pike st. store
+var pike_store = {
+  location: 'pike',
+  min_customers: 23,
+  max_customers: 65,
+  avg_cookies_per_sale: 6.3
+};
+var hold_parent_element = document.getElementById('first-list');
+console.log(hold_parent_element);
+
+var pike_entry = document.createElement('li');
+pike_entry.textContent = pike_store.location;
+
+hold_parent_element.append(pike_entry);
