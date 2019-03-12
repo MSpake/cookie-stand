4fff13a2bcc241d252c407621356bed0597d72e7
mspake <morgana.spake@gmail.com>
2019-03-11 17:02:09 -0700

    hourly cookie sales prints to page as an unordered list, including am/pm

--- js/sales.js
+++ js/sales.js
@@ -1,28 +1,4 @@
 'use strict';
-/*
-TODO: Display the values of each array as unordered lists in the browser
-Calculating the sum of these hourly totals; your output for each location should look like this:
-
-1st and Pike
-
-6am: 16 cookies
-7am: 20 cookies
-8am: 35 cookies
-9am: 48 cookies
-10am: 56 cookies
-11am: 77 cookies
-12pm: 93 cookies
-1pm: 144 cookies
-2pm: 119 cookies
-3pm: 84 cookies
-4pm: 61 cookies
-5pm: 23 cookies
-6pm: 42 cookies
-7pm: 57 cookies
-8pm: 29 cookies
-Total: 657 cookies
-DONE: Display the lists on sales.html. We will be adding features to this application and working with its layout throughout the week.
-*/
 
 //Business Hours
 var business_hours = [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8];
@@ -37,7 +13,8 @@ var pike_store = {
   hours: business_hours,
   cookies_per_hour: [],
   total_cookies_sold: 0,
-  calculate_sales: sales_per_hour
+  calculate_sales: sales_per_hour,
+  hourly_sales: print_hourly_sales
 };
 
 var seatac_store = {
@@ -48,7 +25,8 @@ var seatac_store = {
   hours: business_hours,
   cookies_per_hour: [],
   total_cookies_sold: 0,
-  calculate_sales: sales_per_hour
+  calculate_sales: sales_per_hour,
+  hourly_sales: print_hourly_sales
 };
 
 var seattle_center_store = {
@@ -59,7 +37,8 @@ var seattle_center_store = {
   hours: business_hours,
   cookies_per_hour: [],
   total_cookies_sold: 0,
-  calculate_sales: sales_per_hour
+  calculate_sales: sales_per_hour,
+  hourly_sales: print_hourly_sales
 };
 
 var capitol_hill_store = {
@@ -70,7 +49,8 @@ var capitol_hill_store = {
   hours: business_hours,
   cookies_per_hour: [],
   total_cookies_sold: 0,
-  calculate_sales: sales_per_hour
+  calculate_sales: sales_per_hour,
+  hourly_sales: print_hourly_sales
 };
 
 var alki_store = {
@@ -81,20 +61,10 @@ var alki_store = {
   hours: business_hours,
   cookies_per_hour: [],
   total_cookies_sold: 0,
-  calculate_sales: sales_per_hour
+  calculate_sales: sales_per_hour,
+  hourly_sales: print_hourly_sales
 };
 
-//print to page
-//make it a function
-var add_store_to_sales_list = function(store) {
-  var get_parent_element = document.getElementById('sales-list');
-  var add_store = document.createElement('h2');
-  //remove all whitespace syntax found on Stack Overflow: https://stackoverflow.com/questions/6623231/remove-all-white-spaces-from-text/6623263
-  add_store.setAttribute('id', store.location.toLowerCase().replace(/\s/g, ''));
-  console.log(add_store);
-  add_store.textContent = store.location;
-  get_parent_element.append(add_store);
-};
 
 //generate random number of customers per hour
 //calculate cookie sales for the hour
@@ -106,12 +76,37 @@ function sales_per_hour() {
     var max = Math.floor(this.max_customers);
     var min = Math.ceil(this.min_customers);
     var customers_per_hour = Math.floor(Math.random() * (max - min)) + min;
-    var cookies_per_hour = customers_per_hour * this.avg_cookies_per_sale;
+    var cookies_per_hour = Math.round(customers_per_hour * this.avg_cookies_per_sale);
     this.cookies_per_hour.push(cookies_per_hour);
     this.total_cookies_sold += cookies_per_hour;
   }
 }
 
+//display cookie sales per hour
+//list hourly increments including am/pm
+function print_hourly_sales() {
+  for (var i = 0; i < this.hours.length; i++) {
+    var am_pm;
+    if (i < 6) { am_pm = 'am'; } else { am_pm = 'pm'; } //this will cause problems if the hours change.
+
+    //remove all whitespace syntax found on Stack Overflow: https://stackoverflow.com/questions/6623231/remove-all-white-spaces-from-text/6623263
+    var get_parent_element = document.getElementById(this.location.toLowerCase().replace(/\s/g, ''));
+    var add_hourly = document.createElement('li');
+    add_hourly.textContent = `${this.hours[i]}${am_pm}: ${this.cookies_per_hour[i]} cookies`;
+    get_parent_element.append(add_hourly);
+  }
+}
+
+//function to print given store to the page
+function add_store_to_sales_list(store) {
+  var get_parent_element = document.getElementById('sales-list');
+  var add_store = document.createElement('h2');
+  //remove all whitespace syntax found on Stack Overflow: https://stackoverflow.com/questions/6623231/remove-all-white-spaces-from-text/6623263
+  add_store.setAttribute('id', store.location.toLowerCase().replace(/\s/g, ''));
+  console.log(add_store);
+  add_store.textContent = store.location;
+  get_parent_element.append(add_store);
+}
 
 
 add_store_to_sales_list(pike_store);
@@ -125,3 +120,9 @@ seatac_store.calculate_sales();
 seattle_center_store.calculate_sales();
 capitol_hill_store.calculate_sales();
 alki_store.calculate_sales();
+
+pike_store.hourly_sales();
+seatac_store.hourly_sales();
+seattle_center_store.hourly_sales();
+capitol_hill_store.hourly_sales();
+alki_store.hourly_sales();
