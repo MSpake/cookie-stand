8be1a95d7d88e4a45fdac0e6d1227a9499c262c9
mspake <morgana.spake@gmail.com>
2019-03-13 15:58:20 -0700

    form (with validation) adds new store to table, some basic styling

--- js/sales.js
+++ js/sales.js
@@ -11,6 +11,7 @@ function Cookie_store(location, min_customers, max_customers, avg_cookies_per_sa
   this.max_customers = max_customers;
   this.avg_cookies_per_sale = avg_cookies_per_sale;
   this.hours = business_hours;
+  this.customers_per_hour = [];
   this.cookies_per_hour = [];
   this.total_cookies_sold = 0;
   //remove all whitespace syntax found on Stack Overflow: https://stackoverflow.com/questions/6623231/remove-all-white-spaces-from-text/6623263
@@ -18,15 +19,20 @@ function Cookie_store(location, min_customers, max_customers, avg_cookies_per_sa
 }
 
 //Cookie store method calculate_sales_per_hour
-//generates random number of customers per hour
+//generates random number of customers per hour, pushes to the store's customers_per_hour array
 //calculates cookie sales per hour, pushes to the store's cookies_per_hour array
 //updates total cookies sold for the day
 Cookie_store.prototype.calculate_sales_per_hour = function() {
-  for (var i = 0; i < this.hours.length; i++) {
+  //iterate a number of times equal to the number of hours the store is open
+  for (var i in this.hours) {
+    //generate random number of customers, push to coresponding array
     //syntax for Math.random from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
     var max = Math.floor(this.max_customers);
     var min = Math.ceil(this.min_customers);
     var customers_per_hour = Math.floor(Math.random() * (max - min)) + min;
+    this.customers_per_hour.push(customers_per_hour);
+
+    //calculate cookies sold, push to coresponding arrray
     var cookies_per_hour = Math.round(customers_per_hour * this.avg_cookies_per_sale);
     this.cookies_per_hour.push(cookies_per_hour);
     this.total_cookies_sold += cookies_per_hour;
@@ -36,31 +42,68 @@ Cookie_store.prototype.calculate_sales_per_hour = function() {
 //Cookie store method render_store_to_sales_page
 //renders the store location and hourly sales to the sales table
 Cookie_store.prototype.render_store_to_sales_list = function() {
+  //get parent element.
+  //create a new row with a unique id (store location) and a class 'store'
   var get_parent_element = document.getElementById('sales-list-body');
   var store_row = document.createElement('tr');
   store_row.setAttribute('id', this.id_name);
   store_row.setAttribute('class', 'store');
 
+  //create a new cell contaning the store location and append it to the row
   var store_name = document.createElement('th');
   store_name.textContent = this.location;
   store_row.appendChild(store_name);
 
-  for (var i = 0; i < this.hours.length; i++) {
+  //create cells for each hour that the business is open
+  //each cell contains the number of cookies sold that hour
+  //append cells to the row
+  for (var i in this.hours) {
     var hourly_sales = document.createElement('td');
     hourly_sales.textContent = this.cookies_per_hour[i];
     store_row.appendChild(hourly_sales);
   }
 
+  //create a cell that contains the total cookies sold for the day
+  //append to the row
   var daily_total = document.createElement('td');
   daily_total.textContent = this.total_cookies_sold;
   store_row.appendChild(daily_total);
 
+  //append whole row to the parent element (sales list)
   get_parent_element.appendChild(store_row);
 };
 
 //-----------------------------
 //Stand alone functions
 //-----------------------------
+
+//create_new_store function
+//takes input from an event listner (user input data submited through a form)
+//accesses cookie store constructor to create a new store object using the input values
+//add new store to stores array, calculate new store's sales, render new store to page
+//re-render footer with updated totals
+function create_new_store(event) {
+  event.preventDefault();
+  //access the user input data
+  //assign each of the values for 'location', 'min_customers', 'max_customers', and 'avg_cookies_per_sale' to seperate variables
+  var location = event.target.location.value;
+  var min_customers = event.target.min_customers.value;
+  var max_customers = event.target.max_customers.value;
+  var avg_cookies_per_sale = event.target.avg_cookies_per_sale.value;
+
+  //create new Cookie_store object, push it to the stores array
+  //calculate sales, render store and sales to the page
+  var new_store = new Cookie_store(location, min_customers, max_customers, avg_cookies_per_sale);
+  stores.push(new_store);
+  new_store.calculate_sales_per_hour();
+  new_store.render_store_to_sales_list();
+
+  //re-render the footer with updated totals
+  footer();
+
+  //form field reset syntax found at: https://www.w3schools.com/jsref/met_form_reset.asp
+  event.target.reset();
+}
 
 //header function
 //populates the header of the sales table with the business hours
@@ -69,7 +112,7 @@ function header() {
   var place_holder = document.createElement('th');
   table_head.appendChild(place_holder);
 
-  for (var i = 0; i < business_hours.length; i++) {
+  for (var i in business_hours) {
     var hour = document.createElement('th');
     hour.textContent = business_hours[i];
     table_head.appendChild(hour);
@@ -80,16 +123,17 @@ function header() {
 //populates the footer of the sales table with the daily totals
 function footer() {
   var table_foot = document.getElementById('sales-list-foot');
+  table_foot.innerHTML = '';
   var totals_row = document.createElement('th');
   totals_row.textContent = 'Hourly Totals';
   table_foot.appendChild(totals_row);
 
   var total_all_stores = 0;
 
-  for (var i = 0; i < business_hours.length; i++) {
+  for (var i in business_hours) {
     var total = document.createElement('td');
     var hourly_total = 0;
-    for (var j = 0; j < stores.length; j++) {
+    for (var j in stores) {
       hourly_total += (stores[j].cookies_per_hour[i]);
     }
     total_all_stores += hourly_total;
@@ -120,12 +164,26 @@ var stores = [
 
 //calculate daily sales for each store
 //render store sales to the page
-for (var i = 0; i < stores.length; i++) {
+for (var i in stores) {
   stores[i].calculate_sales_per_hour();
   stores[i].render_store_to_sales_list();
 }
 
 //render header (business hours)
-//render footer (hourly sales)
+//render footer (sales totals)
 header();
 footer();
+
+//when a user submits info for a new store, create a new store, calculate it's sales, and render it to the page
+//includes re-rendering the footer with updated totals
+var form = document.getElementById('enter-new-store');
+form.addEventListener('submit', create_new_store);
+
+//-------------------------------
+//Notes
+
+//TODO:
+//error correction on HTML
+//TODO: stretch:
+//account for variable business hours
+//add employees table
