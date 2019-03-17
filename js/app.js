d6b867a3b8b286bd83cbe2bf52cadd7b341371b9
mspake <morgana.spake@gmail.com>
2019-03-16 23:50:45 -0700

    added ocean theme, sales page fish picture bug resolved

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 'use strict';
 
 function set_color_scheme(event) {
-  console.log(event.target.value);
+  //persisting value using session storage found on Stack Overflow: https://stackoverflow.com/questions/29986657/persist-variables-between-page-loads/30070207#30070207
   sessionStorage.setItem('theme', event.target.value);
   location.reload();
 }
 
 var pages = document.getElementsByClassName('theme-sheet');
-console.log(pages[0]);
+
 
 if (sessionStorage.getItem('theme')) {
   switch (sessionStorage.getItem('theme')) {
@@ -17,14 +17,36 @@ if (sessionStorage.getItem('theme')) {
   case 'greyscale':
     pages[0].innerHTML = '<link rel="stylesheet" href="../css/greyscale.css" class="theme-sheet">';
     break;
-
+  case 'ocean':
+    pages[0].innerHTML = '<link rel="stylesheet" href="../css/ocean.css" class="theme-sheet">';
+    break;
   }
 }
+var fish_pic = document.getElementById('fish');
+console.log(fish_pic);
 
-//persisting value found on Stack Overflow: https://stackoverflow.com/questions/29986657/persist-variables-between-page-loads/30070207#30070207
-console.log(sessionStorage.getItem('theme'));
-
-
+if (fish_pic) {
+  switch (sessionStorage.getItem('theme')) {
+  case 'normal':
+    fish_pic.innerHTML = '';
+    console.log(fish_pic);
+    fish_pic.innerHTML = '<img src="../img/chinook.jpg">';
+    console.log(fish_pic);
+    break;
+  case 'greyscale':
+    fish_pic.innerHTML = '';
+    console.log(fish_pic);
+    fish_pic.innerHTML = '<img src="../img/chinook_greyscale.jpg">';
+    console.log(fish_pic);
+    break;
+  case 'ocean':
+    fish_pic.innerHTML = '';
+    console.log(fish_pic);
+    fish_pic.innerHTML = '<img src="../img/chinook_ocean.jpg">';
+    console.log(fish_pic);
+    break;
+  }
+}
 
 var theme = document.getElementById('themes');
 theme.addEventListener('change', set_color_scheme);
