a6fd7abeea51a031fa27c487b2f57d099277c3fb
mspake <morgana.spake@gmail.com>
2019-03-17 19:09:54 -0700

    added readme info, hopefully fixed a styling bug

--- js/app.js
+++ js/app.js
@@ -1,52 +1,74 @@
 'use strict';
 
+//function to store the user's color scheme selection in session storage
+//persisting a value using session storage found on Stack Overflow: https://stackoverflow.com/questions/29986657/persist-variables-between-page-loads/30070207#30070207
 function set_color_scheme(event) {
-  //persisting value using session storage found on Stack Overflow: https://stackoverflow.com/questions/29986657/persist-variables-between-page-loads/30070207#30070207
   sessionStorage.setItem('theme', event.target.value);
   location.reload();
 }
 
+//if on the main page, this variable will store a value, otherwise it will be null
+var main_page = document.getElementById('main-page');
+
+//get and store the HTML element that contains the link to the CSS style sheet
 var pages = document.getElementsByClassName('theme-sheet');
 
+//if on the sales page, this variable will store the HTML element containing the fish picture there
+var fish_pic = document.getElementById('fish');
 
+//if session storage has a selection value stored in it, set the corresponding theme for all pages
+//currently there are three choices: standard salmon, greyscale, and ocean
 if (sessionStorage.getItem('theme')) {
   switch (sessionStorage.getItem('theme')) {
+  //normal is the standard salmon colors
   case 'normal':
-    pages[0].innerHTML = '<link rel="stylesheet" href="../css/style.css" class="theme-sheet">';
+    //check if on the main page or not
+    //set path for style sheet accordingly
+    //repeated for each case
+    if (main_page) {
+      pages[0].innerHTML = '<link rel="stylesheet" href="./css/style.css" class="theme-sheet">';
+    } else {
+      pages[0].innerHTML = '<link rel="stylesheet" href="../css/style.css" class="theme-sheet">';
+    }
     break;
   case 'greyscale':
-    pages[0].innerHTML = '<link rel="stylesheet" href="../css/greyscale.css" class="theme-sheet">';
+    if (main_page) {
+      pages[0].innerHTML = '<link rel="stylesheet" href="./css/greyscale.css" class="theme-sheet">';
+    } else {
+      pages[0].innerHTML = '<link rel="stylesheet" href="../css/greyscale.css" class="theme-sheet">';
+    }
     break;
   case 'ocean':
-    pages[0].innerHTML = '<link rel="stylesheet" href="../css/ocean.css" class="theme-sheet">';
+    if (main_page) {
+      pages[0].innerHTML = '<link rel="stylesheet" href="./css/ocean.css" class="theme-sheet">';
+    } else {
+      pages[0].innerHTML = '<link rel="stylesheet" href="../css/ocean.css" class="theme-sheet">';
+    }
     break;
   }
 }
-var fish_pic = document.getElementById('fish');
-console.log(fish_pic);
 
+//if on the sales page, set the path to the version of the chinook image that has the correct background color
 if (fish_pic) {
   switch (sessionStorage.getItem('theme')) {
   case 'normal':
     fish_pic.innerHTML = '';
-    console.log(fish_pic);
     fish_pic.innerHTML = '<img src="../img/chinook.jpg">';
-    console.log(fish_pic);
     break;
   case 'greyscale':
     fish_pic.innerHTML = '';
-    console.log(fish_pic);
     fish_pic.innerHTML = '<img src="../img/chinook_greyscale.jpg">';
-    console.log(fish_pic);
     break;
   case 'ocean':
     fish_pic.innerHTML = '';
-    console.log(fish_pic);
     fish_pic.innerHTML = '<img src="../img/chinook_ocean.jpg">';
-    console.log(fish_pic);
     break;
   }
 }
 
+//get and store the HTML element
+//'select' style drop-down list of color schemes
 var theme = document.getElementById('themes');
+
+//when the user selects a color scheme, save their selection into session storage using the set_color_scheme function
 theme.addEventListener('change', set_color_scheme);
